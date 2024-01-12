49d90366a3c551fc3868383ff252a8ee729b4bf4
[Sudip Paudel] <[sudippaudel944@gmail.com]>
2024-01-12 10:31:13 +0545

    Added Questionnaire Form

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+# Used to securely store your API key
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
