0dafc2c0065eb379aef4bbd645ef9e6e2b259177
Bishal Thapa <bishalth77@gmail.com>
2024-01-13 07:26:25 -0800

    models created for progress tracking

--- progress_tracking/models.py
+++ progress_tracking/models.py
@@ -2,6 +2,10 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.auth.models import User
 
-class Progress(models.Model):
-    adhd_levels = ArrayField(models.CharField(max_length=100))
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class  ADHDProgress(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    response_date = models.DateField()
+    score = models.IntegerField()
+
+    class Meta:
+        ordering=['user','response_date']
